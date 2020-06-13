9a425daf182f24e915dd8f7a6ba40ae578a65d79
George <collectionpiter@gmail.com>
2020-06-13 15:02:46 +0300

    card intit

---
+++ src/components/BlockComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="wrapper">
+    <div class="header">
+      <div class="header__sign-wrapper">
+        <font-awesome-icon  :icon="faTimes" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+export default {
+  name: 'BlockComponent',
+  props: {
+    id: Number,
+    title: String,
+    height: Number,
+    width: Number,
+    isCollapsed: Boolean,
+    // coordinates: {
+    //   x: Number,
+    //   y: Number,
+  //  },
+  },
+  components: {
+    FontAwesomeIcon
+  },
+  data() {
+    return {
+      faTimes,
+    }
+  },
+  methods: {
+    
+  }
+
+}
+</script>
+
+
+<style scoped lang="scss">
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    
+    .header {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      height: 50px;
+      background-color: #C5CDB5;
+
+      .header__sign-wrapper {
+        display: flex;
+        justify-content: center;
+        border: 1px solid black;
+        align-items: center;
+        height: 30px;
+        width: 30px;
+        border-radius: 15px;
+        cursor: pointer;
+
+      }
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <block-component msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BlockComponent from '@/components/BlockComponent.vue'
 
 export default {
-  name: 'Home',
+  name: 'Default',
   components: {
-    HelloWorld
+    BlockComponent
   }
 }
 </script>
